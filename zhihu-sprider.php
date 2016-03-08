963e84ea1cf959a234ed54e3c17163fdb5a300ba
sobird <crossyou2009@gmail.com>
2016-03-08 16:23:46 +0000

    打开日志

--- zhihu-sprider.php
+++ zhihu-sprider.php
@@ -108,7 +108,7 @@ function get_user_queue($key = 'index', $count = 10000){
         	}
         }
         foreach ($rows as $row) {
-            //echo $row['username'] . " --- " . date("Y-m-d H:i:s", $row[$key.'_uptime']) . "\n";
+            echo $row['username'] . " --- " . date("Y-m-d H:i:s", $row['index_uptime']) . "\n";
             $redis->lpush($redis_key, $row['username']);
         }
     }
@@ -208,7 +208,7 @@ function saveUserInfo($data) {
 
     $data['ctime'] = time();
 
-    $dbh->save('people', $data, array('username'=>$data['username']));
+    $dbh->save('people_index', $data, array('username'=>$data['username']));
 
     echo "{$data['username']} success...\n";
 }
