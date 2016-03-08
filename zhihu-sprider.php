b404d1a24afa63e2282a5d6b971ea7d5641ef7c9
sobird <crossyou2009@gmail.com>
2016-03-08 17:25:15 +0000

    mysql类库 错误修改

--- zhihu-sprider.php
+++ zhihu-sprider.php
@@ -20,9 +20,7 @@ require 'simple_html_dom.php';
 $http = new Http('http://www.zhihu.com/', array('request_headers' => array('Cookie'=>'_za=3df44f09-34c8-4bd3-8a2b-51f997c1172d; q_c1=74572bdd061d48e5969f57957fa5547b|1456499201000|1451822765000; cap_id="YjRjNjVhNGVjZmM3NDRlODk4ZGVlOTVkZWE2ZmQ0MTQ=|1457356082|5b282e36505a7f76e0c4e9043465ab6aabd4df13"; udid="ADAAHPRflAmPTsGxlcoZ8ZAV5g5bbcIsXTQ="; z_c0="QUFBQUIyRVpBQUFYQUFBQVlRSlZUVHNLQlZmQzFIZDFxWnY4LWJNMEllMmVCNGc4akdkeldnPT0=|1457356091|f44076fe965749f9a284c4e33016b721a68e7e8b"; _xsrf=208a2d65ff8f5110357dc88fe9f8e761; __utmt=1; __utma=51854390.160545789.1457356365.1457445285.1457455662.5; __utmb=51854390.2.10.1457455662; __utmc=51854390; __utmz=51854390.1457445285.4.5.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utmv=51854390.100-1|2=registration_date=20120529=1^3=entry_date=20120529=1')));
 
 
-$dbh = new Mysql('127.0.0.1', 'root', '123456', 'zhihu');
 
-$dbh->set_char();
 
 $dom = new simple_html_dom();
 
@@ -57,7 +55,7 @@ while (pcntl_waitpid(0, $status) != -1) {
 }
 
 function save_user_index() {
-	global $dbh;
+	$dbh = get_dbh();
 	
 	$username = get_user_queue();
 	$progress_id = posix_getpid();
@@ -84,7 +82,7 @@ function save_user_index() {
 }
 
 function get_user_queue($key = 'index', $count = 10000){
-	global $dbh;
+	$dbh = get_dbh();
 	global $seeds;
 
 	$redis = get_redis();
@@ -210,7 +208,7 @@ function get_user_info($url, $data, $offset = 0){
 
 
 function saveUserInfo($data) {
-    global $dbh;
+    $dbh = get_dbh();
 
     $data['ctime'] = time();
 
@@ -238,4 +236,14 @@ function zh_log($msg){
 	echo $msg;
 
     //file_put_contents($this->zh_log_file, $msg, FILE_APPEND | LOCK_EX); }
+}
+
+function get_dbh() {
+	static $instances = array();
+	$key = getmypid();
+	if (empty($instances[$key])){
+		$instances[$key] = new Mysql('127.0.0.1', 'root', '123456', 'zhihu');
+		$instances[$key]->set_char();
+	}
+	return $instances[$key];
 }
