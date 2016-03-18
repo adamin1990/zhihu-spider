aa59339e5471f436bd98317cc5a969c9eeebc7b7
sobird <crossyou2009@gmail.com>
2016-03-18 04:10:30 +0000

    抓取话题

--- topics_sprider2.php
+++ topics_sprider2.php
@@ -10,7 +10,7 @@
 error_reporting(E_ALL);
 
 if (function_exists( 'date_default_timezone_set' )){
-	date_default_timezone_set('UTC');
+    date_default_timezone_set('UTC');
 }
 
 require_once 'Http.class.php';
@@ -25,55 +25,55 @@ $dom = new simple_html_dom();
 worker();
 
 function worker () {
-	$process_count = 8;
-	// 开启8个进程
-	for ($i = 1; $i <= $process_count; $i++) {
-		try {
-			$pid = pcntl_fork();
-			if ($pid == -1) {
-				echo "Could not fork!\n";
-				exit(1);
-			}
-			if (!$pid) {
-				$_pid = getmypid();
-				echo "child process $_pid running\n";
+    $process_count = 8;
+    // 开启8个进程
+    for ($i = 1; $i <= $process_count; $i++) {
+        try {
+            $pid = pcntl_fork();
+            if ($pid == -1) {
+                echo "Could not fork!\n";
+                exit(1);
+            }
+            if (!$pid) {
+                $_pid = getmypid();
+                echo "child process $_pid running\n";
 
-				while(true) {
-					sprider_topic();
-				}
+                while(true) {
+                    sprider_topic();
+                }
 
-				exit($_pid);
-			}
-		} catch(Exception $e) {
+                exit($_pid);
+            }
+        } catch(Exception $e) {
 
-		}
-	}
+        }
+    }
 
-	while (pcntl_waitpid(0, $status) != -1) {
-	    $status = pcntl_wexitstatus($status);
-	    echo "Child $status completed\n";
-	}
+    while (pcntl_waitpid(0, $status) != -1) {
+        $status = pcntl_wexitstatus($status);
+        echo "Child $status completed\n";
+    }
 }
 
 function sprider_topic() {
-	$dbh = get_dbh();
+    $dbh = get_dbh();
 
-	$tid = get_topic_queue();
-	$progress_id = posix_getpid();
+    $tid = get_topic_queue();
+    $progress_id = posix_getpid();
 
-	$time = time();
+    $time = time();
 
-	$dbh->update('topic_index', array('index_uptime'=>$time, 'index_progress_id'=>$progress_id), array('id' => $tid));
+    $dbh->update('topic_index', array('index_uptime'=>$time, 'index_progress_id'=>$progress_id), array('id' => $tid));
 
 
-	crawl_topic($tid);
+    crawl_topic($tid);
 }
 
 function get_topic_queue($count = 10000){
-	$dbh = get_dbh();
-	$redis = get_redis();
+    $dbh = get_dbh();
+    $redis = get_redis();
 
-	$redis_key = 'zhihu_topic_queue';
+    $redis_key = 'zhihu_topic_queue';
 
     // 如果队列为空, 从数据库取一些
     if (!$redis->lsize($redis_key)) {
@@ -83,9 +83,9 @@ function get_topic_queue($count = 10000){
         $rows = $dbh->fetch_all($result);
 
         if(!$rows) {
-        	$sql = "Select `id`, `index_uptime` From `topic_index` Order By `index_uptime` Asc Limit {$count}";
-	        $result = $dbh->query($sql);
-	        $rows = $dbh->fetch_all($result);
+            $sql = "Select `id`, `index_uptime` From `topic_index` Order By `index_uptime` Asc Limit {$count}";
+            $result = $dbh->query($sql);
+            $rows = $dbh->fetch_all($result);
         }
 
         foreach ($rows as $row) {
@@ -97,65 +97,65 @@ function get_topic_queue($count = 10000){
 }
 
 function crawl_topic($tid) {
-	global $http;
-	$dbh = get_dbh();
+    global $http;
+    $dbh = get_dbh();
 
-	$url = 'https://www.zhihu.com/topic/'.$tid .'/top-answers';
+    $url = 'https://www.zhihu.com/topic/'.$tid .'/top-answers';
 
-	$sql = "Select `id`, `index_uptime` From `topic_index` WHERE  `id`=".$tid;
+    $sql = "Select `id`, `index_uptime` From `topic_index` WHERE  `id`=".$tid;
     $result = $dbh->query($sql);
     $rows = $dbh->fetch_all($result);
 
-    if($rows['index_uptime'] !=0) {
-    	return;
+    if($rows && $rows[0]['index_uptime'] !=0) {
+        return;
     }
 
-	$http->get($url, function($body, $headers, $http) {
+    $http->get($url, function($body, $headers, $http) {
         global $dom;
 
         $topics = array();
 
-		$html = $dom->load($body);
+        $html = $dom->load($body);
 
-		$parent_topic = $html->find('.parent-topic div', 0);
+        $parent_topic = $html->find('.parent-topic div', 0);
 
-		$child_topic = $html->find('.child-topic div', 0);
-		
-		if($parent_topic){
-			$parent_topic_c = $parent_topic->children();
-			if($parent_topic_c){
-				foreach ($parent_topic_c as $key => $value) {
-					$tid = $value->getAttribute('data-token');
-					$name = trim($value->text());
+        $child_topic = $html->find('.child-topic div', 0);
+        
+        if($parent_topic){
+            $parent_topic_c = $parent_topic->children();
+            if($parent_topic_c){
+                foreach ($parent_topic_c as $key => $value) {
+                    $tid = $value->getAttribute('data-token');
+                    $name = trim($value->text());
 
-					$topics[] = array(
-						'id' => addslashes($tid),
-			        	'name' => addslashes($name)
-					);
-				}
-			}
-		}
+                    $topics[] = array(
+                        'id' => addslashes($tid),
+                        'name' => addslashes($name)
+                    );
+                }
+            }
+        }
 
-		if($child_topic){
-			$child_topic_c = $child_topic->children();
-			if($child_topic_c){
-				foreach ($child_topic_c as $key => $value) {
-					$tid = $value->getAttribute('data-token');
-					$name = trim($value->text());
+        if($child_topic){
+            $child_topic_c = $child_topic->children();
+            if($child_topic_c){
+                foreach ($child_topic_c as $key => $value) {
+                    $tid = $value->getAttribute('data-token');
+                    $name = trim($value->text());
 
-					$topics[] = array(
-						'id' => addslashes($tid),
-			        	'name' => addslashes($name)
-					);
-				}
-			}
-		}
+                    $topics[] = array(
+                        'id' => addslashes($tid),
+                        'name' => addslashes($name)
+                    );
+                }
+            }
+        }
 
-		foreach ($topics as $key => $value) {
-			save_topic_info($value);
-		}
+        foreach ($topics as $key => $value) {
+            save_topic_info($value);
+        }
 
-	});
+    });
 }
 
 function save_topic_info($data) {
@@ -169,21 +169,21 @@ function save_topic_info($data) {
 }
 
 function get_dbh() {
-	static $instances = array();
-	$key = getmypid();
-	if (empty($instances[$key])){
-		$instances[$key] = new Mysql('127.0.0.1', 'root', 'Yjl&2014', 'zhihu');
-		$instances[$key]->set_char();
-	}
-	return $instances[$key];
+    static $instances = array();
+    $key = getmypid();
+    if (empty($instances[$key])){
+        $instances[$key] = new Mysql('127.0.0.1', 'root', 'Yjl&2014', 'zhihu');
+        $instances[$key]->set_char();
+    }
+    return $instances[$key];
 }
 
 function get_redis() {
-	static $instances = array();
-	$key = getmypid();
-	if (empty($instances[$key])){
-		$instances[$key] = new Redis();
-		$instances[$key]->connect('127.0.0.1', '6379');
-	}
-	return $instances[$key];
+    static $instances = array();
+    $key = getmypid();
+    if (empty($instances[$key])){
+        $instances[$key] = new Redis();
+        $instances[$key]->connect('127.0.0.1', '6379');
+    }
+    return $instances[$key];
 }
